0c89ee739dca9b0ecbabdec002fb26f5b5934ee7
chiaranovales <chi.garcia@stud.uniroma3.it>
2025-09-09 15:26:44 +0200

    primo commit

---
+++ siwProdotto/src/main/java/it/uniroma3/siw/SiwProdottoApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProdottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProdottoApplication.class, args);
+	}
+
+}
